853bb7ab2c5e8989d7d9c6a14eed316588bdfcf4
djand123 <djand123@naver.com>
2024-06-12 17:39:11 +0900

    지역 test

--- src/main/java/com/lec/spring/KTApplication.java
+++ src/main/java/com/lec/spring/KTApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TryTestApplication {
+public class KTApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TryTestApplication.class, args);
+        SpringApplication.run(KTApplication.class, args);
     }
 
 }
