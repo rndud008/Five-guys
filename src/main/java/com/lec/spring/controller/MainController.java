d9e1ecbfdd8e361b9cf51050efcf349861b3c0a3
rndud008 <rndud008@gmail.com>
2024-06-15 17:33:41 +0900

    # 여행정보까지 완료 # LastAPI 아직 미구현

--- src/main/java/com/lec/spring/controller/MainController.java
+++ src/main/java/com/lec/spring/controller/MainController.java
@@ -2,6 +2,8 @@ package com.lec.spring.controller;
 
 import com.lec.spring.service.SigungucodeService;
 import com.lec.spring.service.TravelClassDetailService;
+import com.lec.spring.service.TravelPostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,21 @@ import java.net.URISyntaxException;
 
 @RestController
 public class MainController {
+    @Autowired
     private SigungucodeService sigungucodeService;
+    @Autowired
     private TravelClassDetailService travelClassDetailService;
+    @Autowired
+    private TravelPostService travelPostService;
 
-
+    /*
     public MainController(SigungucodeService sigungucodeService,TravelClassDetailService travelClassDetailService) {
         this.sigungucodeService = sigungucodeService;
         this.travelClassDetailService = travelClassDetailService;
+
     }
+    */
+
 
     @GetMapping("/save")
     public String fetchAndSave() {
@@ -31,10 +40,22 @@ public class MainController {
     }
 
     @GetMapping("/travelType")
-    public String travelTpyeFetchAndSave() {
+    public String travelTypeFetchAndSave() {
         try {
             travelClassDetailService.saveTravelClassDetails();
             return "travelClassDetailServiceData fetched and saved successfully!";
+        } catch (IOException e) {
+            return "Error occurred: " + e.getMessage();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/travelPost")
+    public String travelPostFetchAndSave(){
+        try {
+            travelPostService.saveTravelPosts();
+            return "travelPostServiceData fetched and saved successfully!";
         } catch (IOException e) {
             return "Error occurred: " + e.getMessage();
         } catch (URISyntaxException e) {
--- src/main/java/com/lec/spring/repository/TravelPostRepository.java
+++ src/main/java/com/lec/spring/repository/TravelPostRepository.java
@@ -3,6 +3,7 @@ package com.lec.spring.repository;
 import com.lec.spring.domain.Areacode;
 import com.lec.spring.domain.TravelPost;
 import com.lec.spring.domain.TravelType;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -21,13 +22,7 @@ public interface TravelPostRepository {
     // 특정 여행타입 정보 제목조회 <검색용>
     List<TravelPost> findTravelTypeByModified(TravelType travelType, String modifiedTime);
     // 특정 여행정보 업데이트 리스트 -> last api 활용예정?
-    TravelPost findByContentIdAndType(String contentid, TravelType travelType);
+    TravelPost findByContentIdAndType(String contentid,@Param("travelType") TravelType travelType);
     // 특정 콘텐츠id 여행타입 을 포함한 정보 조회 <블로그,공통,소개,반복 api 활용예정.>
-
-
-    // 지역기반 정보 조회 url
-    // https://apis.data.go.kr/B551011/KorService1/areaBasedList1?serviceKey=%s&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&contentTypeId=%d
-
-    //
 
 }
