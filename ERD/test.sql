719fa8bd6509059827b6a1c9705989091dc79c24
answnsgh99 <junhogg@naver.com>
2024-06-25 09:32:41 +0900

    625 (1)

--- ERD/test.sql
+++ ERD/test.sql
@@ -1,229 +1,233 @@
-INSERT INTO areacode (areacode, name, regId) VALUES
- (1, '서울', '11B10101'),
- (2, '인천', '11B20201'),
- (3, '대전', '11C20401'),
- (4, '대구', '11H10701'),
- (5, '광주', '11F20501'),
- (6, '부산', '11H20201'),
- (7, '울산', '11H20101'),
- (8, '세종', '11C20404'),
- (31, '경기', '11B10101'),  -- 서울의 regId를 경기에 포함
- (32, '강원', '11D10101'),  -- 철원의 regId를 강원에 포함
- (33, '충북', '11C10101'),  -- 충주의 regId를 충북에 포함
- (34, '충남', '11C20101'),  -- 서산의 regId를 충남에 포함
- (35, '경북', '11H10101'),  -- 울진의 regId를 경북에 포함
- (36, '경남', '11H20301'),  -- 창원의 regId를 경남에 포함
- (37, '전북', '11F10201'),  -- 전주의 regId를 전북에 포함
- (38, '전남', '11F20101'),  -- 함평의 regId를 전남에 포함
- (39, '제주', '11G00201');
 
-select * from areacode;
-
-SELECT name
-FROM areacode
-WHERE areacode = 9;
-
+-- areacode 테이블 데이터 확인
 SELECT *
-FROM sigungucode;
+FROM areacode;
 
-INSERT INTO travel_diary_post (user_id, areacode, subject, content) VALUES
-(1, 1, '제목1', '내용1'),
-(1, 2, '제목2', '내용2'),
-(2, 1, '제목3', '내용3'),
-(2, 2, '제목4', '내용4'),
-(3, 1, '제목5', '내용5'),
-(3, 2, '제목6', '내용6'),
-(1, 2, '제목7', '내용7'),
-(2, 1, '제목8', '내용8')
-;
--- 페이징 실습용 다량의 데이터
-INSERT INTO travel_diary_post(user_id, areacode_id, subject, content)
-SELECT user_id, areacode_id, subject, content FROM travel_diary_post;
+SELECT s.*
+FROM sigungucode s;
 
-SELECT * FROM travel_diary_post ORDER BY 1 asc ;
-SELECT  count(*) FROM travel_diary_post;
+-- 서울의 areacode와 이름 조회
+SELECT a.name, s.name
+FROM areacode a,
+     sigungucode s
+WHERE a.areacode = 1
+  and s.areacode = 1;
 
-DELETE FROM travel_diary_post;
-ALTER TABLE travel_diary_post Auto_Increment = 1;
 
-INSERT INTO user (username, password, name, email) VALUES
-('USER1', '1234', '회원1', 'user1@mail.com'),
-('USER2', '1234', '회원2', 'user2@mail.com'),
-('ADMIN1', '1234', '관리자1', 'admin1@mail.com')
-;
-
-SELECT  * FROM user;
-
+-- short_weather 테이블 데이터 조회
 SELECT *
-FROM user
-WHERE id = 3;
+FROM short_weather;
 
-
-SELECT * FROM travel_diary_post;
-
-INSERT INTO travel_diary_post (user_id, areacode_id, subject, content)
-VALUES (3, 2, '제목', '내용');
-
-UPDATE travel_diary_post
-SET content = 'test', subject = 'test'
-WHERE id = 1;
-
-DELETE FROM travel_diary_post
-WHERE id = 1;
-
-SELECT *
-FROM travel_diary_post p, areacode a
-WHERE p.areacode_id = a.areacode AND p.areacode_id = 1;
+-- middle_weather 테이블 데이터 조회
+select *
+from areacode;
 
 SELECT name
 FROM areacode
 WHERE areacode = 1;
 
-SELECT * FROM comment;
-SELECT * FROM travel_diary_post;
-
-INSERT INTO comment (user_id, travel_diary_post_id, content) VALUES
-(1, 1, '1번유저 1번글의 댓글'),
-(1, 2, '1번유저 2번글의 댓글'),
-(2, 1, '2번유저 1번글의 댓글'),
-(2, 2, '2번유저 2번글의 댓글'),
-(3, 1, '3번유저 1번글의 댓글'),
-(3, 2, '3번유저 2번글의 댓글');
-
-DELETE FROM comment;
-ALTER TABLE comment Auto_Increment = 1;
-
-DELETE FROM travel_diary_post;
-ALTER TABLE travel_diary_post Auto_Increment = 1;
-
-SELECT c.id, c.user_id, c.travel_diary_post_id, c.content, c.regdate
-FROM comment c, travel_diary_post p
-WHERE c.user_id = p.id AND c.travel_diary_post_id = 2;
-
-SELECT
-    c.id "c_id",
-    c.content "c_content",
-    c.regdate "c_regdate",
-    c.travel_diary_post_id "c_post_id",
-    u.id "u_id",
-    u.username "u_username",
-    u.password "u_password",
-    u.name "u_name",
-    u.email "u_email",
-    u.regdate "u_regdate"
-FROM comment c, user u
-WHERE c.user_id = u.id AND c.travel_diary_post_id = 1
-ORDER BY c.id DESC;
-
-SELECT * FROM comment;
-
-SELECT
-    p.id "p_id",
-    p.subject "p_subject",
-    p.content "p_content",
-    p.viewcnt "p_viewcnt",
-    p.regdate "p_regdate",
-    u.id "u_id",
-    u.username "u_username",
-    u.name "u_name",
-    u.email "u_email",
-    u.regdate "u_regdate"
-FROM travel_diary_post p, user u
-WHERE p.user_id = u.id AND p.areacode_id = 1
-ORDER BY p.id DESC
-    LIMIT 5, 5;
-
-SELECT
-    c.id "c_id",
-    c.content "c_content",
-    c.regdate "c_regdate",
-    c.travel_diary_post_id "c_post_id",
-    u.id "u_id",
-    u.username "u_username",
-    u.password "u_password",
-    u.name "u_name",
-    u.email "u_email",
-    u.regdate "u_regdate"
-FROM comment c, user u
-WHERE c.user_id = u.id AND c.travel_diary_post_id = 1
-ORDER BY c.id DESC;
-
-INSERT INTO attachment(travel_diary_post_id, sourcename, filename) VALUES
-(1, 'face01.png', 'face01.png'),
-(1, 'face02.png', 'face02.png'),
-(2, 'face03.png', 'face03.png'),
-(2, 'face04.png', 'face04.png'),
-(3, 'face05.png', 'face05.png'),
-(3, 'face06.png', 'face06.png'),
-(4, 'face07.png', 'face07.png'),
-(4, 'face08.png', 'face08.png')
-;
-DELETE FROM attachment;
-ALTER TABLE attachment Auto_Increment = 1;
-
-SELECT * FROM attachment;
-
-SELECT * FROM user_travel_diary_post;
-SELECT * FROM user_travel_diary_post WHERE travel_diary_post_id = 2;
-
-INSERT INTO user_travel_diary_post
-    (user_id, travel_diary_post_id)
-VALUES
-    (1, 1),
-    (1, 2),
-    (1, 3),
-    (2, 1),
-    (3, 1)
-    ;
-
-SELECT * FROM user_authorities;
-
-DELETE FROM user_travel_diary_post;
-ALTER TABLE user_travel_diary_post AUTO_INCREMENT = 1;
-# WHERE user_id = 1 AND travel_diary_post_id = 2;
+SELECT *
+FROM sigungucode
+where areacode = 2
+  and sigungucode = 3;
 
 SELECT *
-FROM user_travel_diary_post
-WHERE user_id = 1 AND travel_diary_post_id = 1;
-
-SELECT count(*)
-FROM user_travel_diary_post
-WHERE travel_diary_post_id = 1;
-
-INSERT INTO user ( username, password, name, email) VALUES
-    ('USER4', '$2a$10$8HVLMZgJLqQTHcFgxgQiSu2FwpUUAHALnW7Iq0PFXkWAXe.Pv6Qqe', '관리자4', 'junho@naver.com');
-
-SELECT * FROM user;
-
-SELECT id, name
-FROM authority
-WHERE id=1;
-
-SELECT * FROM authority;
-
-INSERT INTO authority (id, name)
-VALUES (1, 'ROLE_ADMIN'), (2, 'ROLE_MEMBER');
-
-
-DELETE FROM authority;
-
-insert into user_authorities(user_id, authority_id) VALUES
-    (5, 1);
+FROM sigungucode
+where areacode = 37;
 
 select *
-from user_authorities;
+FROM travel_type;
 
-delete
-from user_authorities
+select *
+FROM last_call_api_date;
+
+select *
+FROM travel_class_detail;
+
+select *
+FROM travel_class_detail
+where decode = '';
+
+select infotext
+FROM travel_post
+where travel_class_detail_id = 15;
+
+select *
+FROM travel_post
+where contentid = 127377;
+
+select *
+FROM blog_review;
+
+select *
+FROM travel_post
+where contentid = 294390;
+
+select *
+FROM travel_post;
+
+select *
+FROM travel_post
+where title LIKE '%도드람%';
+
+select *
+FROM last_call_api_date;
+
+select tp.*
+FROM travel_post tp
+         join travel_class_detail tcd on tp.travel_class_detail_id = tcd.id
+         join travel_type tt on tcd.travel_type_id = tt.id
+where tt.id = 15
+  and infotext is null;
+
+select tp.*
+FROM travel_post tp
+         join travel_class_detail tcd on tp.travel_class_detail_id = tcd.id
+         join travel_type tt on tcd.travel_type_id = tt.id
+where title like '%계곡%'
+  and travel_type_id = 12;
+
+
+SELECT *
+FROM last_call_api_date;
+
+INSERT INTO last_call_api_date (id, url, regdate)
+VALUES (1, 'test', '2024-06-12');
+
+select *
+FROM travel_class_detail tcd
+         join travel_type tt on tcd.travel_type_id = tt.id
+where travel_type_id = 12;
+
+DROP TABLE IF EXISTS weather_forecast;
+
+ALTER TABLE short_weather
+    MODIFY COLUMN last_call_api_id INT DEFAULT 0;
+
+select *
+from weather_forecast;
+
+SELECT *
+FROM weather_forecast
+WHERE forecast_date = '2024-06-16';
+
+SELECT *
+FROM weather_forecast
+WHERE forecast_date = '2024-06-16'
+  AND category = 'tmp';
+
+
+CREATE TABLE last_call_api_date
+(
+    id      INT  NOT NULL AUTO_INCREMENT COMMENT 'API 호출 id',
+    url     TEXT NULL COMMENT 'apiurl',
+    regdate DATE NULL DEFAULT (CURRENT_DATE()) COMMENT '호출 날짜',
+    PRIMARY KEY (id)
+) COMMENT 'api 중복호출 방지';
+
+CREATE TABLE weather_forecast
+(
+    id             INT AUTO_INCREMENT PRIMARY KEY,
+    category       VARCHAR(50),
+    forecast_date  DATE,
+    forecast_time  VARCHAR(10),
+    forecast_value VARCHAR(50),
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    url            TEXT
+
+);
+
+#     api_call_id INT,
+#     CONSTRAINT fk_api_call_id FOREIGN KEY (api_call_id) REFERENCES last_call_api_date(id)
+
+select *
+from short_weather;
+
+select tp.id                     as tp_id,
+       tp.travel_class_detail_id AS tp_travel_class_detail_id,
+       tp.last_call_api_id       AS tp_last_call_api_id,
+       tp.sigungucode_id         AS tp_sigungucode_id,
+       tp.title                  AS tp_title,
+       tp.addr1                  AS tp_addr1,
+       tp.addr2                  AS tp_addr2,
+       tp.contentid              AS tp_contentid,
+       tp.firstimage             AS tp_firstimage,
+       tp.firstimage2            AS tp_firstimage2,
+       tp.cpyrhtDivCd            AS tp_cpyrhtDivCd,
+       tp.mapx                   AS tp_mapx,
+       tp.mapy                   AS tp_mapy,
+       tp.modifiedtime           AS tp_modifiedtime,
+       tp.tel                    AS tp_tel,
+       tp.eventstartdate         AS tp_eventstartdate,
+       tp.eventenddate           AS tp_eventenddate,
+       tp.infocenter             AS tp_infocenter,
+       tp.parking                AS tp_parking,
+       tp.restdate               AS tp_restdate,
+       tp.usetime                AS tp_usetime,
+       tp.homepage               AS tp_homepage,
+       tp.overview               AS tp_overview,
+       tp.eventplace             AS tp_eventplace,
+       tp.playtime               AS tp_playtime,
+       tp.usetimefestival        AS tp_usetimefestival,
+       tp.infoname               AS tp_infoname,
+       tp.infotext               AS tp_infotext,
+       tcd.id                   AS tcd_id,
+       tcd.travel_type_id       AS tcd_travel_type_id,
+       tcd.name                 AS tcd_name,
+       tcd.code                 AS tcd_code,
+       tcd.decode               AS tcd_decode,
+       tt.id                     AS tt_id,
+       tt.name                   AS tt_name,
+       s.id                      AS s_id,
+       s.sigungucode             AS s_sigungucode,
+       s.name                    AS s_name,
+       s.areacode                AS s_areacode,
+       a.areacode                AS a_areacode,
+       a.name                    AS a_name,
+       a.regId                   AS a_regId,
+       lcad.id                   AS lcad_id,
+       lcad.url                  AS lcad_url,
+       lcad.regdate              AS lcad_regdate
+FROM travel_post tp
+         JOIN travel_class_detail tcd on tcd.id = tp.travel_class_detail_id
+         JOIN travel_class_detail tcd2 ON tcd.decode = tcd2.code
+         JOIN travel_type tt ON tcd.travel_type_id = tt.id
+         JOIN sigungucode s ON tp.sigungucode_id = s.id
+         JOIN areacode a ON s.areacode = a.areacode
+         JOIN last_call_api_date lcad ON tp.last_call_api_id = lcad.id
+where
+    tt.id= 12
+# and
+#     tcd2.decode = 'a02'
+#     a.areacode =1
+# and
+#     s.sigungucode = 1
+# and
+#     STR_TO_DATE(tp.eventstartdate, '%Y%m%d') >= CURDATE()
+# order by tp.eventstartdate asc
+  and
+    tp.homepage = ''
+
 ;
 
-select *
-from user;
 
-delete
-from user;
+SELECT distinct
+    tcd.*
+FROM
+    travel_class_detail tcd
+        LEFT JOIN
+    travel_class_detail tcd2 ON tcd.decode = tcd2.code
+        join travel_type tt on tcd.travel_type_id = tt.id
+# where
+# tcd2.code = 'a0101'
+#   and tcd2.decode = 'a01'
+#   and
+#     tcd2.code = 'a0101'
+#   and tcd3.code = 'a01011300'
+#   and
+#   tcd3.travel_type_id = 12
+;
 
-desc user_authorities;
 
-select * from information_schema.table_constraints where table_name = 'user';
-select * from information_schema.table_constraints where constraint_schema = 'travel' ;
+
+
