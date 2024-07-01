6bb39b18ac1bbdb1d0fb2de808fb79fb0b0c11c8
rndud008 <rndud008@gmail.com>
2024-07-01 14:09:28 +0900

    ### 7/1 수정완료

--- src/main/java/com/lec/spring/controller/HomeController.java
+++ src/main/java/com/lec/spring/controller/HomeController.java
@@ -3,12 +3,10 @@ package com.lec.spring.controller;
 import com.lec.spring.config.PrincipalDetails;
 import com.lec.spring.domain.*;
 import com.lec.spring.service.BlogReviewService;
-import com.lec.spring.service.LastCallApiDateService;
 import com.lec.spring.service.TravelPostService;
 import com.lec.spring.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -18,35 +16,26 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Controller
 public class HomeController {
-    @Value("${app.apikey}")
-    private String apikey;
+
+
+    @Autowired
+    private TravelPostService travelPostService;
+    @Autowired
+    private BlogReviewService blogReviewService;
 
     private UserService userService;
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
     }
-
-    @Autowired
-    private TravelPostService travelPostService;
-    @Autowired
-    private BlogReviewService blogReviewService;
-    @Autowired
-    private LastCallApiDateService lastCallApiDateService;
 
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal UserDetails userDetails) {
@@ -60,103 +49,6 @@ public class HomeController {
         }
 
         return "home";
-    }
-
-    @GetMapping("/post/{id}")
-    public String post(@PathVariable String id, Model model, @AuthenticationPrincipal UserDetails userDetails) throws IOException {
-
-        if (userDetails != null) {
-            User loggedUser = ((PrincipalDetails) userDetails).getUser();
-            model.addAttribute("loggedUser", loggedUser);
-        } else {
-            model.addAttribute("loggedUser", null);
-        }
-
-        TravelPost travelPost = travelPostService.getTravelPostBycontentId(id);
-
-        String apiUrl = String.format("https://apis.data.go.kr/B551011/KorService1/" +
-                "detailCommon1?serviceKey=%s" +
-                "&MobileOS=ETC&MobileApp=AppTest&_type=json&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&numOfRows=10&pageNo=1" +
-                "&contentId=%s&contentTypeId=%d", apikey, travelPost.getContentid(), travelPost.getTravelClassDetail().getTravelType().getId());
-
-
-        LocalDate today = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String formattedDate = today.format(formatter);
-
-        if (lastCallApiDateService.selectedByUrlAndRegDate(apiUrl, formattedDate) == null) {
-            LastCallApiDate lastCallApiDate = new LastCallApiDate();
-            lastCallApiDate.setUrl(apiUrl);
-            lastCallApiDateService.save(lastCallApiDate);
-
-            travelPost = travelPostService.update(travelPost,lastCallApiDate);
-        }
-
-        travelPost.setHomepage(extraUrl(travelPost.getHomepage()));
-        System.out.println(travelPost.getHomepage());
-        model.addAttribute("post", travelPost);
-
-        List<BlogReview> blogReviewList = blogReviewService.selectedTravelPostByBlogReview(travelPost, 0, 5);
-
-        model.addAttribute("blogReview", blogReviewList);
-
-        return "post";
-    }
-
-    @GetMapping("/festival/{id}") //495
-    public String festival(@PathVariable String id, Model model, @AuthenticationPrincipal UserDetails userDetails) throws IOException {
-
-        if (userDetails != null) {
-            User loggedUser = ((PrincipalDetails) userDetails).getUser();
-            model.addAttribute("loggedUser", loggedUser);
-        } else {
-            model.addAttribute("loggedUser", null);
-        }
-
-        TravelPost travelPost = travelPostService.getTravelPostBycontentId(id);
-
-        String apiUrl = String.format("https://apis.data.go.kr/B551011/KorService1/" +
-                "detailCommon1?serviceKey=%s" +
-                "&MobileOS=ETC&MobileApp=AppTest&_type=json&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&numOfRows=10&pageNo=1" +
-                "&contentId=%s&contentTypeId=%d", apikey, travelPost.getContentid(), travelPost.getTravelClassDetail().getTravelType().getId());
-
-
-        LocalDate today = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String formattedDate = today.format(formatter);
-
-        if (lastCallApiDateService.selectedByUrlAndRegDate(apiUrl, formattedDate) == null) {
-            LastCallApiDate lastCallApiDate = new LastCallApiDate();
-            lastCallApiDate.setUrl(apiUrl);
-            lastCallApiDateService.save(lastCallApiDate);
-
-            travelPost = travelPostService.update(travelPost,lastCallApiDate);
-        }
-
-        travelPost.setHomepage(extraUrl(travelPost.getHomepage()));
-        model.addAttribute("post", travelPost);
-
-
-        List<BlogReview> blogReviewList = blogReviewService.selectedTravelPostByBlogReview(travelPost, 0, 5);
-        model.addAttribute("blogReview", blogReviewList);
-
-
-        return "festival";
-    }
-
-    public String extraUrl(String homepage) {
-        if (homepage == null || homepage.isEmpty()) {
-            return "";
-        }
-
-        Pattern pattern = Pattern.compile("\"(http[^\"]*)\"");
-        Matcher matcher = pattern.matcher(homepage);
-
-        if (matcher.find()) {
-            return matcher.group(1);
-        }
-
-        return "";
     }
 
     /**
@@ -229,10 +121,6 @@ public class HomeController {
     public String rejectAuth() {
         return "common/rejectAuth";
     }
-
-
-
-
 
     @RestController
     public class BlogRestController {
--- src/main/java/com/lec/spring/controller/TravelInfomationController.java
+++ src/main/java/com/lec/spring/controller/TravelInfomationController.java
@@ -2,96 +2,136 @@ package com.lec.spring.controller;
 
 import com.lec.spring.config.PrincipalDetails;
 import com.lec.spring.domain.BlogReview;
+import com.lec.spring.domain.LastCallApiDate;
 import com.lec.spring.domain.TravelPost;
 import com.lec.spring.domain.User;
 import com.lec.spring.service.BlogReviewService;
+import com.lec.spring.service.LastCallApiDateService;
 import com.lec.spring.service.TravelPostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Controller
-@RequestMapping("/categoryTable")
 public class TravelInfomationController {
-//
-//    @Autowired
-//    TravelPostService travelPostService;
-//    @Autowired
-//    BlogReviewService blogReviewService;
-//
-//    @GetMapping("/12/post/{contentId}")
-//    public String post(@PathVariable String contentId, Model model, @AuthenticationPrincipal UserDetails userDetails) throws IOException {
-//
-//        if(userDetails != null){
-//            User loggedUser = ((PrincipalDetails) userDetails).getUser();
-//            model.addAttribute("loggedUser", loggedUser);
-//        }else {
-//            model.addAttribute("loggedUser", null);
-//        }
-//
-//        TravelPost travelPost = travelPostService.getTravelPostBycontentId(contentId);
-//
-//        travelPost = travelPostService.update(travelPost);
-//
-//        travelPost.setHomepage(extraUrl(travelPost.getHomepage()));
-//        System.out.println(travelPost.getHomepage());
-//        model.addAttribute("post", travelPost);
-//
-//        List<BlogReview> blogReviewList = blogReviewService.selectedTravelPostByBlogReview(travelPost, 0, 5);
-//
-//        model.addAttribute("blogReview",blogReviewList);
-//
-//        return "post";
-//
-//    }
-//
-//    @GetMapping("/15/festival/{id}") //495
-//    public String festival(@PathVariable String id, Model model, @AuthenticationPrincipal UserDetails userDetails) throws IOException {
-//
-//        if(userDetails != null){
-//            User loggedUser = ((PrincipalDetails) userDetails).getUser();
-//            model.addAttribute("loggedUser", loggedUser);
-//        }else {
-//            model.addAttribute("loggedUser", null);
-//        }
-//
-//        TravelPost travelPost = travelPostService.getTravelPostBycontentId(id);
-//
-//        travelPost = travelPostService.update(travelPost);
-//
-//        travelPost.setHomepage(extraUrl(travelPost.getHomepage()));
-//        model.addAttribute("post", travelPost);
-//
-//
-//        List<BlogReview> blogReviewList = blogReviewService.selectedTravelPostByBlogReview(travelPost, 0, 5);
-//        model.addAttribute("blogReview",blogReviewList);
-//
-//        return "festival";
-//    }
-//
-//    public String extraUrl(String homepage){
-//        if(homepage == null || homepage.isEmpty()){
-//            return "";
-//        }
-//
-//        Pattern pattern = Pattern.compile("\"(http[^\"]*)\"");
-//        Matcher matcher = pattern.matcher(homepage);
-//
-//        if (matcher.find()) {
-//            return matcher.group(1);
-//        }
-//
-//        return "";
-//    }
+    @Value("${app.apikey}")
+    private String apikey;
+
+    @Autowired
+    private TravelPostService travelPostService;
+    @Autowired
+    private BlogReviewService blogReviewService;
+    @Autowired
+    private LastCallApiDateService lastCallApiDateService;
+
+    @GetMapping("/post/{id}")
+    public String post(@PathVariable String id, Model model, @AuthenticationPrincipal UserDetails userDetails) throws IOException {
+
+        if (userDetails != null) {
+            User loggedUser = ((PrincipalDetails) userDetails).getUser();
+            model.addAttribute("loggedUser", loggedUser);
+        } else {
+            model.addAttribute("loggedUser", null);
+        }
+
+        TravelPost travelPost = travelPostService.getTravelPostBycontentId(id);
+
+        String apiUrl = String.format("https://apis.data.go.kr/B551011/KorService1/" +
+                "detailCommon1?serviceKey=%s" +
+                "&MobileOS=ETC&MobileApp=AppTest&_type=json&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&numOfRows=10&pageNo=1" +
+                "&contentId=%s&contentTypeId=%d", apikey, travelPost.getContentid(), travelPost.getTravelClassDetail().getTravelType().getId());
+
+
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate = today.format(formatter);
+
+        if (lastCallApiDateService.selectedByUrlAndRegDate(apiUrl, formattedDate) == null) {
+            LastCallApiDate lastCallApiDate = new LastCallApiDate();
+            lastCallApiDate.setUrl(apiUrl);
+            lastCallApiDateService.save(lastCallApiDate);
+
+            travelPost = travelPostService.update(travelPost,lastCallApiDate);
+        }
+
+        travelPost.setHomepage(extraUrl(travelPost.getHomepage()));
+        System.out.println(travelPost.getHomepage());
+        model.addAttribute("post", travelPost);
+
+        List<BlogReview> blogReviewList = blogReviewService.selectedTravelPostByBlogReview(travelPost, 0, 5);
+
+        model.addAttribute("blogReview", blogReviewList);
+
+        return "post";
+    }
+
+    @GetMapping("/festival/{id}") //495
+    public String festival(@PathVariable String id, Model model, @AuthenticationPrincipal UserDetails userDetails) throws IOException {
+
+        if (userDetails != null) {
+            User loggedUser = ((PrincipalDetails) userDetails).getUser();
+            model.addAttribute("loggedUser", loggedUser);
+        } else {
+            model.addAttribute("loggedUser", null);
+        }
+
+        TravelPost travelPost = travelPostService.getTravelPostBycontentId(id);
+
+        String apiUrl = String.format("https://apis.data.go.kr/B551011/KorService1/" +
+                "detailCommon1?serviceKey=%s" +
+                "&MobileOS=ETC&MobileApp=AppTest&_type=json&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&numOfRows=10&pageNo=1" +
+                "&contentId=%s&contentTypeId=%d", apikey, travelPost.getContentid(), travelPost.getTravelClassDetail().getTravelType().getId());
+
+
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate = today.format(formatter);
+
+        if (lastCallApiDateService.selectedByUrlAndRegDate(apiUrl, formattedDate) == null) {
+            LastCallApiDate lastCallApiDate = new LastCallApiDate();
+            lastCallApiDate.setUrl(apiUrl);
+            lastCallApiDateService.save(lastCallApiDate);
+
+            travelPost = travelPostService.update(travelPost,lastCallApiDate);
+        }
+
+        travelPost.setHomepage(extraUrl(travelPost.getHomepage()));
+        model.addAttribute("post", travelPost);
+
+
+        List<BlogReview> blogReviewList = blogReviewService.selectedTravelPostByBlogReview(travelPost, 0, 5);
+        model.addAttribute("blogReview", blogReviewList);
+
+
+        return "festival";
+    }
+
+    public String extraUrl(String homepage) {
+        if (homepage == null || homepage.isEmpty()) {
+            return "";
+        }
+
+        Pattern pattern = Pattern.compile("\"(http[^\"]*)\"");
+        Matcher matcher = pattern.matcher(homepage);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return "";
+    }
+
 
 }
