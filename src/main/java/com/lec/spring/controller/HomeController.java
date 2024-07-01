0e0abd6cb0faf36f66cf074e910147431f4bcc5c
Yoon-Sung-Jung <mrj951230@naver.com>
2024-07-01 12:04:57 +0900

    240701 TravelPostService method명 수정

--- src/main/java/com/lec/spring/controller/HomeController.java
+++ src/main/java/com/lec/spring/controller/HomeController.java
@@ -55,7 +55,7 @@ public class HomeController {
             model.addAttribute("loggedUser", null);
         }
 
-        TravelPost travelPost = travelPostService.getTravelPostById(id);
+        TravelPost travelPost = travelPostService.getTravelPostBycontentId(id);
 
         travelPost = travelPostService.update(travelPost);
 
@@ -80,7 +80,7 @@ public class HomeController {
             model.addAttribute("loggedUser", null);
         }
 
-        TravelPost travelPost = travelPostService.getTravelPostById(id);
+        TravelPost travelPost = travelPostService.getTravelPostBycontentId(id);
 
         travelPost = travelPostService.update(travelPost);
 
@@ -196,7 +196,7 @@ public class HomeController {
         @GetMapping("/api/festival/blogs")
         public List<BlogReview> getMoreBlogs(@RequestParam("id") String id, @RequestParam("offset") int offset) throws IOException {
 
-            TravelPost travelPost = travelPostService.getTravelPostById(id);
+            TravelPost travelPost = travelPostService.getTravelPostBycontentId(id);
             System.out.println(blogReviewService.selectedTravelPostByBlogReview(travelPost, offset, 3));
 
             return blogReviewService.selectedTravelPostByBlogReview(travelPost, offset, 3);
@@ -205,7 +205,7 @@ public class HomeController {
         @GetMapping("/api/festival/blogs/count")
         public List<BlogReview> getMoreBlogs2(@RequestParam("id") String id) throws IOException {
 
-            TravelPost travelPost = travelPostService.getTravelPostById(id);
+            TravelPost travelPost = travelPostService.getTravelPostBycontentId(id);
 
             return blogReviewService.getsumBlogReview(travelPost);
         }
